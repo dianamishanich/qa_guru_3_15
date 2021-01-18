f7986e418d643675aeee4b6c3faae41607f7278b
dianamishanich <diana-mishanich@ya.ru>
2021-01-19 00:09:02 +0300

    CarbonspaceTest

--- src/test/java/qa_guru_3_15/CarbonspaceTest.java
+++ src/test/java/qa_guru_3_15/CarbonspaceTest.java
@@ -1,42 +1,70 @@
 package qa_guru_3_15;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.aeonbits.owner.ConfigFactory;
 
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CarbonspaceTest {
-    //Авторизация и открытие главной страницы
-    @Test
-    @DisplayName("Авторизация и открытие главной страницы")
-    public void authorizationTest() {
-        ErnestHemingway config = ConfigFactory.newInstance().create(ErnestHemingway.class);
+    ErnestHemingway config = ConfigFactory.newInstance().create(ErnestHemingway.class);
 
-        String userLogin = config.userLogin(),
-               userPassword = config.userPassword();
+    String userLogin = config.userLogin(),
+           userPassword = config.userPassword(),
+           year = "2016",
+           month = "July",
+           year0 = "2000",
+           year1 = "2016";
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
+
+    @BeforeEach
+    public void logIn() {
         open("https://my.carbonspace.tech/");
-        $("input[placeholder='Login']").val(userLogin);
-        $("input[placeholder='Password']").val(userPassword);
+        $("input[name='username']").val(userLogin);
+        $("input[name='password']").val(userPassword);
         $(".ant-btn.ant-btn-primary").click();
+    }
 
+    @Test
+    @DisplayName("Главное меню и страницы")
+    public void authorizationTest() {
+
+        $(byText("Carbon Profile")).click();
+        $(".ui.card.page-content").shouldHave(text("Area"));
+        $(byText("Carbon Analytics")).click();
+        $(".header-title").shouldHave(text("Carbonspace Grünes Herz GmbH"));
 
     }
-    //Главное меню
-    /*@Test
-    @DisplayName("Главное меню")
-    void mainMenuTest() {
-        open("https://my.carbonspace.tech/");
-        $$(".ui large top fixed menu").byText("Carbon Profile").click;
 
-    }*/
+    @Test
+    @DisplayName("Выбор периода времени в фильтре Carbon Map")
+    public void CarbonMapFilterTest(){
+    open("https://my.carbonspace.tech/map");
+    $(".main_dropdown_year").$(".ui.inline.dropdown").click();
+    $(".visible.menu.transition").$(byText(year)).click();
+    $(".main_dropdown_month").$(".ui.inline.dropdown").click();
+    $(".visible.menu.transition").$(byText(month)).click();
+    $(".counts").shouldHave(text("-7.213Mt"));
+    }
 
-    //Выбор периода времени в фильтре CarbonMap
-    //Маштабирование и определение текущей координаты на CarbonMap
-    //Аналитика с выбором параметров в Carbon Analytics
-    //Выбор сегментов в Carbon Profile
-    //Личный кабинет: изменение пароля/выход
-
+    @Test
+    @DisplayName("Аналитика с выбором параметров в Carbon Analytics")
+    public void CarbonAnalyticFilterTest(){
+        open("https://my.carbonspace.tech/analytics");
+        $(".ui.container.dropdowns").click();
+        $(".visible.menu.transition").$(byText(year0)).scrollTo().click();
+        $(".ui.container.dropdowns").sibling(1).click();
+        $(".visible.menu.transition").$(byText(year1)).scrollTo().click();
+        $(".recharts-cartesian-axis-ticks").shouldHave(text(year0), text(year1));
+    }
 
 }
